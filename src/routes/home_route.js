9bcb80702bc77b8021207628cca055881a135b64
VŨ ĐỨC HOÀNG <Hoangbarca1492@gmail.com>
2023-06-26 21:19:04 +0700

    them https

--- src/routes/home_route.js
+++ src/routes/home_route.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { uid } = require('uid')
 require('dotenv').config()
-
+const port = process.env.PORT
 
 module.exports = router.get('/home', (req, res) => {
     console.log(req.ip);
-    // console.log(req.hostname);
-    const path = 'ws://' + req.hostname + ':8080'
+
+    const path = req.protocol === 'http' ? ('ws://' + req.hostname + ':' + port) : ('wss://' + req.hostname + ':' + port)
     const next = uid(32);
     res.render('home', { isHome: true, next: next, support: true, path: path, ip: req.ip })
 })  
--- src/routes/room_route.js
+++ src/routes/room_route.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const { uid } = require('uid')
 require('dotenv').config()
 
+const port = process.env.PORT
 
 
 module.exports = router.get('/room/:id', (req, res) => {
-    const path = 'ws://' + req.hostname + ':8080'
+    const path = req.protocol === 'http' ? ('ws://' + req.hostname + ':' + port) : ('wss://' + req.hostname + ':' + port)
     const next = uid(32)
     res.render('room', { roomId: req.params.id, isHome: false, next: next, support: true, path: path })
 })  
